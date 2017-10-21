9cf8c438c43239660078564fc8dcf13535532e52
Smith <2543405307@qq.com>
2017-10-21 16:15:23 +0800

    基本完成

--- 耀耀学院/任务七：UI组件之排序表格/js.js
+++ 耀耀学院/任务七：UI组件之排序表格/js.js
@@ -1,34 +1,55 @@
 window.onload = function() {
     var table = {
-            "th": ['姓名', '语文', '数学', '英语', '总分'],
-            "tbody": [
-                ['张三', 80, 70, 90, 240],
+            //sortable属性决定了是否可以排序，也就是是否加小箭头
+            "th": [ 
+                { "value": '姓名' },
+                { "value": '语文', "sortable": false }, 
+                { "value": '数学', "sortable": false },
+                { "value": '英语', "sortable": false },
+                { "value": '总分', "sortable": false }
+            ],
+            //表格内容
+            "tbody": [ 
                 ['李四', 80, 90, 100, 280],
+                ['张三', 80, 70, 90, 240],
                 ['王五', 99, 99, 100, 298]
             ],
-            "sortTh": ['语文', '数学', '英语', '总分'],
-            "sortFun": null
+            //排序方法
+            "sortFun": function(a, b) { 
+                if (a < b) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
         },
         th = table.th,
         tbody = table.tbody,
-        sortTh = table.sortTh,
         thHtml = '',
-        tbodyHtml = '';
+        tbodyHtml = '',
+        sortFun = table.sortFun;
+    initialTh();
+    initialTbody();
 
-    for (var i = 0; i < th.length; i++) {
-        if (sortTh.indexOf(th[i]) > -1) {
-            thHtml += '<th>' + th[i] + "<span class='ascending'></span><span class='descending'></span></th>";
-        } else {
-            thHtml += '<th>' + th[i] + "</th>";
+    function initialTh() {
+        for (var i = 0; i < th.length; i++) {
+            if (typeof th[i].sortable != 'undefined') {
+                thHtml += '<th>' + th[i].value + "<span class='ascending'></span><span class='descending'></span></th>";
+
+            } else {
+                thHtml += '<th>' + th[i].value + "</th>";
+            }
         }
     }
 
-    for (var k = 0; k < tbody.length; k++) {
-        tbodyHtml = tbodyHtml + '<tr>';
-        for (var j = 0; j < tbody[k].length; j++) {
-            tbodyHtml = tbodyHtml + '<td>' + tbody[k][j] + '</td>';
+    function initialTbody() {
+        for (var k = 0; k < tbody.length; k++) {
+            tbodyHtml = tbodyHtml + '<tr>';
+            for (var j = 0; j < tbody[k].length; j++) {
+                tbodyHtml = tbodyHtml + '<td>' + tbody[k][j] + '</td>';
+            }
+            tbodyHtml = tbodyHtml + '</tr>';
         }
-        tbodyHtml = tbodyHtml + '</tr>';
     }
 
     var tableEle;
@@ -36,37 +57,46 @@ window.onload = function() {
     tableEle.id = "jsTable";
     tableEle.innerHTML = thHtml + tbodyHtml;
     document.body.appendChild(tableEle);
-    /*console.log(tableEle.id);*/
-    tableEle.onclick = function(event) {
+
+    tableEle.onclick = function(event, sortFun) {
         event = event || window.event;
-        var target = event.target || event.srcElement;
+        var target = event.target || event.srcElement,
+            isSortBtn = (target.className === 'ascending') || (target.className === 'descending');
 
-        if (target.className === 'ascending' || target.className === 'descending') {
-            var th=Array.prototype.slice.call(tableEle.getElementsByTagName('th'));
-            var indexTh=th.indexOf(target.parentNode);
-            console.log(indexTh);
-            if (target.className === 'ascending') {
+        if (isSortBtn) {
+            var sortTh = Array.prototype.slice.call(tableEle.getElementsByTagName('th')),
+                sortThIndex = sortTh.indexOf(target.parentNode),
+                targetTbody = [],
+                sortTd = [];
 
-            } else if (target.className === 'descending') {
-
+            for (var i = tbody.length - 1; i >= 0; i--) {
+                sortTd.push(tbody[i][sortThIndex]);
             }
 
+            if (target.className === 'ascending') {
+                sortTd.sort(sortFun);
+                sortTh[sortThIndex].sortable = 'ascending';
+            } else if (target.className === 'descending') {
+                sortTd.sort(sortFun).reverse();
+                sortTh[sortThIndex].sortable = 'descending';
+            }
+
+            //倒叙遍历，倒叙添加新项
+
+            for (var i = sortTd.length - 1; i >= 0; i--) {
+                for (var j = tbody.length - 1; j >= 0; j--) {
+                    if (sortTd[i] === tbody[j][sortThIndex]) {
+                        targetTbody.unshift(tbody[j]);
+                        tbody.splice(j, 1);
+                        break;
+                    }
+                }
+            }
+
+            tbody = targetTbody;
+            tbodyHtml = '';
+            initialTbody();
+            tableEle.innerHTML = thHtml + tbodyHtml;
         }
     };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 };
